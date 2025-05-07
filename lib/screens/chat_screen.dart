d89b2bdd8a8d311e667c02298b0b7519a19fdd4a
Ba-jx <bayan.ghanem161@gmail.com>
2025-05-07 17:49:58 +0300

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+// Keep your imports unchanged
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -36,7 +37,7 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   String _getChatId(String a, String b) =>
-      a.hashCode <= b.hashCode ? '${a}_$b' : '${b}_$a';
+      a.hashCode <= b.hashCode ? '${a}$b' : '${b}$a';
 
   Future<void> _markMessagesAsRead() async {
     final messages = await FirebaseFirestore.instance
@@ -71,7 +72,6 @@ class _ChatScreenState extends State<ChatScreen> {
         .add(message);
 
     _controller.clear();
-    _scrollToBottom();
   }
 
   void _scrollToBottom() {
@@ -84,6 +84,63 @@ class _ChatScreenState extends State<ChatScreen> {
         );
       }
     });
+  }
+
+  Widget _buildMessageBubble(Map<String, dynamic> msg, bool isMe) {
+    final timestamp = msg['timestamp'] as Timestamp?;
+    final timeText = timestamp != null
+        ? TimeOfDay.fromDateTime(timestamp.toDate()).format(context)
+        : '';
+
+    return Align(
+      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: isMe ? Colors.deepPurple[300] : Colors.grey[300],
+          borderRadius: BorderRadius.only(
+            topLeft: const Radius.circular(12),
+            topRight: const Radius.circular(12),
+            bottomLeft: Radius.circular(isMe ? 12 : 0),
+            bottomRight: Radius.circular(isMe ? 0 : 12),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          children: [
+            Text(
+              msg['message'] ?? '',
+              style: TextStyle(color: isMe ? Colors.white : Colors.black),
+            ),
+            const SizedBox(height: 4),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  timeText,
+                  style: TextStyle(
+                    fontSize: 10,
+                    color: isMe ? Colors.white70 : Colors.black54,
+                  ),
+                ),
+                if (isMe)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4),
+                    child: Icon(
+                      msg['isRead'] == true ? Icons.done_all : Icons.check,
+                      size: 14,
+                      color: msg['isRead'] == true
+                          ? Colors.white
+                          : Colors.white60,
+                    ),
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   bool _shouldShowDateDivider(DateTime current, DateTime? previous) {
@@ -140,48 +197,7 @@ class _ChatScreenState extends State<ChatScreen> {
                               style: const TextStyle(color: Colors.grey),
                             ),
                           ),
-                        Align(
-                          alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                            padding: const EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: isMe ? Colors.purple[300] : Colors.grey[300],
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(msg['message'] ?? ''),
-                                const SizedBox(height: 4),
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      timestamp != null
-                                          ? DateFormat.jm().format(timestamp)
-                                          : '',
-                                      style: const TextStyle(fontSize: 10, color: Colors.black54),
-                                    ),
-                                    if (isMe)
-                                      Padding(
-                                        padding: const EdgeInsets.only(left: 4),
-                                        child: Icon(
-                                          msg['isRead'] == true
-                                              ? Icons.done_all
-                                              : Icons.check,
-                                          size: 14,
-                                          color: msg['isRead'] == true
-                                              ? Colors.deepPurple
-                                              : Colors.black45,
-                                        ),
-                                      ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
+                        _buildMessageBubble(msg, isMe),
                       ],
                     );
                   },
@@ -194,20 +210,30 @@ class _ChatScreenState extends State<ChatScreen> {
             child: Row(
               children: [
                 Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      hintText: 'Type your message...',
-                      border: OutlineInputBorder(),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(24),
+                      border: Border.all(color: Colors.grey[300]!),
                     ),
-                    onSubmitted: (_) => _sendMessage(),
+                    child: TextField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                        hintText: 'Type your message...',
+                        border: InputBorder.none,
+                      ),
+                      onSubmitted: (_) => _sendMessage(),
+                    ),
                   ),
                 ),
                 const SizedBox(width: 8),
-                IconButton(
-                  onPressed: _sendMessage,
-                  icon: const Icon(Icons.send),
-                  color: Theme.of(context).primaryColor,
+                CircleAvatar(
+                  backgroundColor: Colors.deepPurple,
+                  child: IconButton(
+                    onPressed: _sendMessage,
+                    icon: const Icon(Icons.send, color: Colors.white),
+                  ),
                 ),
               ],
             ),
