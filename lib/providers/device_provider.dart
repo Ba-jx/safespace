5596594bf0dabdd7ffe4b121b787e06728b5b956
zeinali1 <zein.ali1@yahoo.com>
2025-05-13 21:51:46 +0300

    Update device_provider.dart

--- lib/providers/device_provider.dart
+++ lib/providers/device_provider.dart
@@ -4,23 +4,28 @@ class DeviceProvider extends ChangeNotifier {
   int _heartRate = 0;
   double _temperature = 0.0;
   int _oxygenLevel = 0;
+  DateTime? _lastUpdated;
 
   int get heartRate => _heartRate;
   double get temperature => _temperature;
   int get oxygenLevel => _oxygenLevel;
+  DateTime? get lastUpdated => _lastUpdated;
 
   void updateHeartRate(int value) {
     _heartRate = value;
+    _lastUpdated = DateTime.now();
     notifyListeners();
   }
 
   void updateTemperature(double value) {
     _temperature = value;
+    _lastUpdated = DateTime.now();
     notifyListeners();
   }
 
   void updateOxygenLevel(int value) {
     _oxygenLevel = value;
+    _lastUpdated = DateTime.now();
     notifyListeners();
   }
 
@@ -40,6 +45,7 @@ class DeviceProvider extends ChangeNotifier {
           _temperature = double.tryParse(tempStr) ?? _temperature;
         }
       }
+      _lastUpdated = DateTime.now();
       notifyListeners();
     } catch (e) {
       debugPrint('Error parsing Bluetooth data: $e');
@@ -50,6 +56,7 @@ class DeviceProvider extends ChangeNotifier {
     _heartRate = 0;
     _temperature = 0.0;
     _oxygenLevel = 0;
+    _lastUpdated = null;
     notifyListeners();
   }
 }
