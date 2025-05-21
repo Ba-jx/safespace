fd03d23d0087d2684a0c06c45105c7b7db3b8498
zeinali1 <zein.ali1@yahoo.com>
2025-05-21 20:56:57 +0300

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -8,59 +8,49 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
-  late AnimationController _fadeController;
-  late AnimationController _scaleController;
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
   late Animation<double> _fadeAnimation;
-  late Animation<double> _scaleAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    _fadeController = AnimationController(
+    _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 1500),
-    );
-    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(_fadeController);
-
-    _scaleController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1200),
-    );
-    _scaleAnimation = Tween<double>(begin: 0.6, end: 1).animate(
-      CurvedAnimation(parent: _scaleController, curve: Curves.easeOutBack),
+      duration: const Duration(seconds: 2),
     );
 
-    _fadeController.forward();
-    _scaleController.forward();
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(_controller);
 
-    Timer(const Duration(seconds: 5), () {
+    _controller.forward();
+
+    Timer(const Duration(seconds: 4), () {
       Navigator.pushReplacementNamed(context, '/role-selection');
     });
   }
 
   @override
   void dispose() {
-    _fadeController.dispose();
-    _scaleController.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
-      backgroundColor: const Color(0xFFF5F5FF),
+      backgroundColor: isDark ? Colors.black : Colors.white,
       body: Center(
         child: FadeTransition(
           opacity: _fadeAnimation,
-          child: ScaleTransition(
-            scale: _scaleAnimation,
-            child: Image.asset(
-              'assets/images/safe_space_logo1.png',
-              height: 180, // Increased size
-              width: 180,
-              fit: BoxFit.contain,
+          child: Text(
+            'Safe Space',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.purple[400],
+              letterSpacing: 1.5,
             ),
           ),
         ),
